746baf0c2e993681acacde94d4a0e176b7776adb
mariotey <tey.mingchuan@yahoo.com.sg>
2022-10-14 16:43:27 +0800

    Updated test_file.py

--- test_file.py
+++ test_file.py
@@ -4,4 +4,4 @@ N = 5
 
 X_train, y_train, X_test, y_test, Ytr, Yts, Ptrain_list, Ptest_list, w_list, error_train_array, error_test_array = grading.A2_A0220997U(N)
 
-print("Complete")
+print("Complete\n")
