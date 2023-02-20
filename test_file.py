6a5cde820922711c5826193ef96bcc53faebe397
mariotey <39178091+mariotey@users.noreply.github.com>
2023-02-20 10:54:36 +0800

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -3,6 +3,6 @@ import A3.A3_A0220997U as grading
 learning_rate = 0.1
 num_iters = 10
 
-a_out, f1_out, b_out, f2_out, c_out, d_out, f3_out = grading.A3_A0220997U(learning_rate, num_iters)
+a_out, f1_out, b_out, f2_out, c_out, d_out, f3_out = grading(learning_rate, num_iters)
 
 print("Complete\n")
